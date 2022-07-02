d777930338fe4c5d8e44c470832c9ea28a8135c9
akayberk <by4berk@gmail.com>
2022-07-02 06:46:07 -0400

    task3 has been committed and published as wanted.

--- src/task3.java
+++ src/task3.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 public class task3 {
 
     public static void main(String[] args) {
-        float a,b,c;
+        float a,b,c,u,alan;
         Scanner input = new Scanner(System.in); //scanner function'i calistirmak icin cagirdigimiz kod.
-        System.out.println("Dik ucgene ait ilk dik kenarin uzunlugunu: ");
+        System.out.println("Ucgene ait ilk kenarin uzunlugunu: ");
         a = input.nextFloat();
-        System.out.println("Dik ucgene ait ikinci dik kenarin uzunlugu: ");
+        System.out.println("Ucgene ait ikinci kenarin uzunlugunu: ");
         b = input.nextFloat();
-        c = (float) Math.sqrt((a*a)+(b*b));
-        System.out.println("Dik ucgene ait hipotenus uzunlugu= " + c);
+        System.out.println("Ucgene ait ilk kenarin uzunlugunu: ");
+        c = input.nextFloat();
+        u = (a+b+c)*0.5f;
+        alan = (float) Math.sqrt(u*(u-a)*(u-b)*(u-c));
+        System.out.println("Alan= " + alan);
     }
 }
